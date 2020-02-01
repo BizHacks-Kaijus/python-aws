fe8ebba0534d456287fee2d414a587dcaa24af3b
Zenith08 <kaidenr@shaw.ca>
2020-02-01 01:54:20 -0800

    Combined comprehend.

--- src/comprehend.py
+++ src/comprehend.py
@@ -2,7 +2,7 @@ import boto3
 
 import json
 
-comprehend = boto3.client('comprehend')
+comprehend = boto3.client('comprehend', region_name='us-east-1')
 
 text = "The Lenovo T420 is a high performance business class notebook with a second gen Intel i5 processor, 4GB of Ram and a 320 GB HDD. This high quality, refurbished notebook is perfect for any computing environment, including at home for personal use or small and medium sized businesses looking to expand their fleet at a reasonable price point. The Lenovo T420 is built with high quality tier 1 components and has been professionally refurbished to ensure you?re getting a unit in Grade A condition. This desktop includes a 1 year warranty and is fully loaded with a Windows 10 Pro operating system."
 print('Detecting key phrases')
