64eb472b8f5cba29820f818aa77ea6cbd4701af7
kricha02 <t5z2b@ugrad.cs.ubc.ca>
2020-02-01 03:55:59 -0800

    Finished WebScraper

--- src/WebScraper.py
+++ src/WebScraper.py
@@ -1,12 +1,16 @@
 import urllib.request
 import json
+import csv
 
 prefix = "https://www.bestbuy.ca/api/v2/json/product/"
 suffix = "?currentRegion=BC&include=all&lang=en-CA"
 
 #start 10000000 end 99999999
 
-for i in range(12785800, 12785910):
+outputFile = open("data.csv", 'w') #load csv file
+output = csv.writer(outputFile) #create a csv.write  
+
+for i in range(10000000, 99999999):
     print(prefix + str(i) + suffix)
     try:
         with urllib.request.urlopen(prefix + str(i) + suffix) as url:
@@ -14,5 +18,6 @@ for i in range(12785800, 12785910):
             # I'm guessing this would output the html source code ?
             js = json.loads(s)
             print(js['name'])
+            output.writerow(js.values()) #values row
     except:
         print("not found")
